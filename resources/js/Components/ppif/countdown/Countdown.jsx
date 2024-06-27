2e553263f8ef9e2cac83f9b694bf1115ea3a6e2d
michael tio <tio.michael2004@gmail.com>
2024-06-27 14:53:50 +0700

    nyoba bikin countdown

---
+++ resources/js/Components/ppif/countdown/Countdown.jsx
@@ -0,0 +1,55 @@
+import React, { useState, useEffect } from "react";
+
+const CountdownTimer = ({ targetDate }) => {
+    const calculateTimeLeft = () => {
+        const difference = +new Date(targetDate) - +new Date();
+        let timeLeft = {};
+
+        if (difference > 0) {
+            timeLeft = {
+                days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+                hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+                minutes: Math.floor((difference / 1000 / 60) % 60),
+                seconds: Math.floor((difference / 1000) % 60),
+            };
+        }
+
+        return timeLeft;
+    };
+
+    const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setTimeLeft(calculateTimeLeft());
+        }, 1000);
+
+        return () => clearTimeout(timer);
+    });
+
+    const timerComponents = [];
+
+    Object.keys(timeLeft).forEach((interval) => {
+        if (!timeLeft[interval]) {
+            return;
+        }
+
+        timerComponents.push(
+            <span key={interval} className="mx-2 text-2xl">
+                {timeLeft[interval]} {interval}{" "}
+            </span>,
+        );
+    });
+
+    return (
+        <div className="flex justify-center">
+            {timerComponents.length ? (
+                timerComponents
+            ) : (
+                <span className="text-2xl">Time's up!</span>
+            )}
+        </div>
+    );
+};
+
+export default CountdownTimer;
