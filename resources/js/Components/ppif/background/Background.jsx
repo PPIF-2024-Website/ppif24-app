7d3a5a5ae17f6f85d5cc1dc85dd4c867079bc11f
michael tio <tio.michael2004@gmail.com>
2024-07-05 20:42:36 +0700

    upload gambar

--- resources/js/Components/ppif/background/Background.jsx
+++ resources/js/Components/ppif/background/Background.jsx
@@ -2,12 +2,17 @@ import React from "react";
 
 function Background() {
     return (
-        <div className="absolute w-screen">
+        <div className="absolute w-full overflow-x-hidden bg-black">
             <div className="h-screen"></div>
             <div className="h-screen"></div>
             <div className="h-screen"></div>
             <div className="h-screen"></div>
-            <div className="h-screen"></div>
+            <div className="h-screen">
+                <img
+                    className="absolute bottom-0 right-0 h-screen w-full"
+                    src="/ppif/images/background/Vector-14.svg"
+                />
+            </div>
         </div>
     );
 }
--- resources/js/Pages/BackgroundTest.jsx
+++ resources/js/Pages/BackgroundTest.jsx
@@ -5,19 +5,19 @@ function BackgroundTest() {
     return (
         <>
             <Background />
-            <div className="z-1 border-1 relative flex h-screen items-center justify-center border-white text-white">
+            <div className="z-1 relative flex h-screen items-center justify-center border-2 border-green-500 text-green-500">
                 Landing
             </div>
-            <div className="z-1 relative flex h-screen items-center justify-center text-white">
+            <div className="z-1 relative flex h-screen items-center justify-center border-2 border-green-500 text-green-500">
                 Home Section
             </div>
-            <div className="z-1 relative flex h-screen items-center justify-center text-white">
+            <div className="z-1 relative flex h-screen items-center justify-center border-2 border-green-500 text-green-500">
                 About PPIF
             </div>
-            <div className="z-1 relative flex h-screen items-center justify-center text-white">
+            <div className="z-1 relative flex h-screen items-center justify-center border-2 border-green-500 text-green-500">
                 Time Line
             </div>
-            <div className="z-1 relative flex h-screen items-center justify-center text-white">
+            <div className="z-1 relative flex h-screen items-center justify-center border-2 border-green-500 text-green-500">
                 Contact Us
             </div>
         </>
