75ff1b5801777ed8b957978057d243cbabfb5b3c
alessfd <alessandro.fayezdevanno@gmail.com>
2024-07-06 16:53:13 +0700

    timeline page

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,10 @@ Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('/timeline', function () {
+    return Inertia::render('TimelinePage');
+});
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
