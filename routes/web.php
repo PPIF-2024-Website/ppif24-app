155b0bca495deb175231d386c9f7e530b562688a
H0tt4ru <stenlieangelonathan@gmail.com>
2024-07-08 20:46:01 +0700

    Fixing Routes

--- routes/web.php
+++ routes/web.php
@@ -14,10 +14,6 @@ Route::get('/', function () {
     ]);
 });
 
-Route::get('/home', function () {
-    return Inertia::render('Home');
-});
-
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
