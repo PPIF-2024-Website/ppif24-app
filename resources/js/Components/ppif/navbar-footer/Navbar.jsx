6be4d1a325bf4a6cf8ac49cd85a94622412430e9
H0tt4ru <stenlieangelonathan@gmail.com>
2024-07-08 17:35:57 +0700

    Major fixes, responsive

--- resources/js/Components/ppif/navbar-footer/Navbar.jsx
+++ resources/js/Components/ppif/navbar-footer/Navbar.jsx
@@ -1,13 +1,86 @@
 import { Link } from "@inertiajs/react";
+import { useEffect, useState } from "react";
 
 export default function Navbar() {
+    const [isScrollUp, setIsScrollUp] = useState(true);
+    const [isBurgerActive, setIsBurgerActive] = useState(false);
+
+    useEffect(() => {
+        let lastScroll = window.scrollY;
+
+        const handleScroll = () => {
+            const scroll = window.scrollY;
+
+            if (scroll < lastScroll) {
+                setIsScrollUp(true);
+            } else {
+                setIsScrollUp(false);
+            }
+
+            lastScroll = scroll;
+        };
+
+        window.addEventListener("scroll", handleScroll);
+
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, []);
+
+    const handleBurger = () => {
+        setIsBurgerActive(!isBurgerActive);
+    };
+
     return (
-        <nav className="fixed mt-8 flex space-x-16 rounded-xl bg-white bg-opacity-20 px-12 py-4 text-white shadow-[0px_0px_10px_0px_rgba(255,255,255,1)] backdrop-blur-lg">
-            <Link>Home</Link>
-            <Link>About IF</Link>
-            <Link>About PPIF</Link>
-            <Link>Timeline</Link>
-            <Link>Contact</Link>
-        </nav>
+        <div className="flex w-screen justify-center">
+            <div className="fixed z-50 flex h-16 w-screen justify-end md:hidden">
+                <img
+                    onClick={handleBurger}
+                    className="z-50 m-2 max-w-96"
+                    src="/ppif/icons/burger_navigation.svg"
+                    alt=""
+                ></img>
+            </div>
+            <nav
+                className={`${isScrollUp ? "md:top-0" : "md:-top-40"} ${isBurgerActive ? "top-0" : "-top-[100vh]"} fixed z-10 mx-auto flex h-full w-screen flex-col bg-white bg-opacity-10 pt-16 text-2xl text-white backdrop-blur-md transition-all duration-200 ease-in-out md:mt-8 md:flex md:h-auto md:w-auto md:flex-row md:gap-16 md:rounded-xl md:px-12 md:py-5 md:text-base md:shadow-[0px_0px_50px_20px_rgba(255,255,255,0.1)]`}
+            >
+                <hr className="opacity-20 md:hidden" />
+                <Link
+                    className="p-4 transition-all hover:drop-shadow-[0px_0px_5px_rgba(255,255,255,1)] md:p-0"
+                    href="#Home"
+                >
+                    Home
+                </Link>
+                <hr className="opacity-20 md:hidden" />
+                <Link
+                    className="p-4 transition-all hover:drop-shadow-[0px_0px_5px_rgba(255,255,255,1)] md:p-0"
+                    href="#AboutIF"
+                >
+                    About IF
+                </Link>
+                <hr className="opacity-20 md:hidden" />
+                <Link
+                    className="p-4 transition-all hover:drop-shadow-[0px_0px_5px_rgba(255,255,255,1)] md:p-0"
+                    href="#AboutPPIF"
+                >
+                    About PPIF
+                </Link>
+                <hr className="opacity-20 md:hidden" />
+                <Link
+                    className="p-4 transition-all hover:drop-shadow-[0px_0px_5px_rgba(255,255,255,1)] md:p-0"
+                    href="#Timeline"
+                >
+                    Timeline
+                </Link>
+                <hr className="opacity-20 md:hidden" />
+                <Link
+                    className="p-4 transition-all hover:drop-shadow-[0px_0px_5px_rgba(255,255,255,1)] md:p-0"
+                    href="#Contact"
+                >
+                    Contact
+                </Link>
+                <hr className="opacity-20 md:hidden" />
+            </nav>
+        </div>
     );
 }
--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,30 +1,17 @@
 import { Head } from "@inertiajs/react";
 import Navbar from "@/Components/ppif/navbar-footer/Navbar";
+import Landing from "@/Components/ppif/home/Landing";
 
 export default function Home() {
     return (
         <>
             <Head title="Home" />
-            <div className="flex h-screen w-screen justify-center">
-                <Navbar className="" />
-                <div className="flex h-screen w-screen items-center justify-center bg-black">
-                    <div className="absolute flex h-screen w-screen items-center justify-center">
-                        <img
-                            className="max-w-32 lg:w-full"
-                            src="/ppif/images/logo-ppif.png"
-                            alt="ppif"
-                        />
-                    </div>
-                    <div className="absolute bottom-12 flex flex-col items-center space-y-4 text-white">
-                        <p className="">click to scroll down</p>
-                        <img
-                            className=""
-                            src="/ppif/icons/arrow-down.svg"
-                            alt=""
-                        />
-                    </div>
-                </div>
-            </div>
+            <Navbar />
+            <Landing id={"Home"} />
+            <Landing id={"AboutIF"} />
+            <Landing id={"AboutPPIF"} />
+            <Landing id={"Timeline"} />
+            <Landing id={"Contact"} />
         </>
     );
 }
