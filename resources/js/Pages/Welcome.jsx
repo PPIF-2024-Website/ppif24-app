ca31b9429e714f8e5e07a863886cad35ec778cdd
michael tio <tio.michael2004@gmail.com>
2024-07-10 20:03:03 +0700

    menambah background component sementara

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -1,11 +1,13 @@
 import { Head } from "@inertiajs/react";
 import Navbar from "@/Components/ppif/navbar-footer/Navbar";
 import Landing from "@/Components/ppif/landing/Landing";
+import Background from "@/Components/ppif/background/Background";
 
 export default function Welcome() {
     return (
         <>
             <Head title="Home" />
+            <Background />
             <Navbar />
             <Landing id={"Home"} />
             <Landing id={"AboutIF"} />
