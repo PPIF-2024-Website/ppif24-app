624f35caee7b975751fd5ea246d50792ea42c806
michael tio <tio.michael2004@gmail.com>
2024-07-12 00:08:16 +0700

    nyoba css manual untuk main background

--- resources/js/Components/ppif/background/Background.jsx
+++ resources/js/Components/ppif/background/Background.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Background() {
     return (
-        <div className="absolute w-full overflow-x-hidden bg-gradient-to-b from-[#14000B] from-60% via-[#0E0600] via-20% to-[#00000E] to-20%">
+        <div className="main-background absolute w-full overflow-x-hidden">
             <div className="h-screen">
                 {/* <img
                     className="h-screen w-full min-w-[1000px] object-fill"
