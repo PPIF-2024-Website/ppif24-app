1e6157cdbc39e16320d4977ce7f791cbc21bd67e
michael tio <tio.michael2004@gmail.com>
2024-07-11 23:49:33 +0700

    update background

--- resources/js/Components/ppif/background/Background.jsx
+++ resources/js/Components/ppif/background/Background.jsx
@@ -2,36 +2,36 @@ import React from "react";
 
 function Background() {
     return (
-        <div className="absolute w-full overflow-x-hidden bg-black">
+        <div className="absolute w-full overflow-x-hidden bg-gradient-to-b from-[#14000B] from-60% via-[#0E0600] via-20% to-[#00000E] to-20%">
             <div className="h-screen">
-                <img
+                {/* <img
                     className="h-screen w-full min-w-[1000px] object-fill"
                     src="/ppif/images/background/section1.png"
-                />
+                /> */}
             </div>
             <div className="h-screen">
-                <img
+                {/* <img
                     className="min-w h-screen w-full object-fill"
                     src="/ppif/images/background/section2.png"
-                />
+                /> */}
             </div>
             <div className="h-screen">
-                <img
+                {/* <img
                     className="h-screen w-full object-fill"
                     src="/ppif/images/background/section3.png"
-                />
+                /> */}
             </div>
             <div className="h-screen">
-                <img
+                {/* <img
                     className="h-screen w-full object-fill"
                     src="/ppif/images/background/section4.png"
-                />
+                /> */}
             </div>
             <div className="h-screen">
-                <img
+                {/* <img
                     className="h-screen w-full object-fill"
                     src="/ppif/images/background/section5.png"
-                />
+                /> */}
             </div>
         </div>
     );
