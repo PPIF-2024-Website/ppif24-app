690b325f9c98f93629b99d65a374c6ede0f32440
oscarjiro <oscarjiroj@gmail.com>
2024-07-23 15:08:21 +0700

    fix: replace alt text with role for UMN building background

--- resources/js/Components/ppif/background/AnimatedBackground.jsx
+++ resources/js/Components/ppif/background/AnimatedBackground.jsx
@@ -20,7 +20,7 @@ export default function AnimatedBackground() {
                 <img
                     className="mask-bg absolute left-0 h-full w-full object-cover opacity-80 xl:object-fill"
                     src="/ppif/images/background/umn-bg.png"
-                    alt="Gedung UMN"
+                    role="presentation"
                 />
                 {/* <img
                     className="min-w h-screen object-cover xl:w-full xl:object-fill"
