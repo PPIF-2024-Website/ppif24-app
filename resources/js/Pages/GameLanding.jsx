169362b2b2cf5259c6fba43e62efcdff6c7e82d9
JSenpa1 <41582843+JSenpa1@users.noreply.github.com>
2024-07-23 20:26:35 +0700

    commit

--- resources/js/Pages/GameLanding.jsx
+++ resources/js/Pages/GameLanding.jsx
@@ -7,9 +7,9 @@ function GameLanding() {
   return (
     <div className="h-screen w-screen">
         <ArchivesBackground />
-        <div className="flex flex-col justify-center items-center h-screen">
+        <div className="flex flex-col justify-center items-center h-screen gap-6">
             <Game_story_card image = "/ppif/images/story_1.png" />
-            <Link href="/Game" className="bg-white/20 flex justify-center items-center backdrop-blur-lg text-center text-white mt-4 w-[389px] md:w-[778px] md:h-[76px] rounded-[15px] text-[15px] md:text-[30px] transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:bg-ppif-orange duration-300">Continue</Link>
+            <Link href="/Game" className="bg-white/20 flex justify-center items-center backdrop-blur-lg text-center text-white w-[300px] md:w-[600px] md:h-[60px] h-[40px] rounded-[15px] text-[15px] md:text-[24px] transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:bg-ppif-orange duration-300">Continue</Link>
         </div>
     </div>
   )
