a43edadeb75d7ddf4335c90f92da1b83bbce31f0
oscarjiro <oscarjiroj@gmail.com>
2024-07-23 15:11:42 +0700

    fix: optimize UMN building background
    - Set fetch priority to low
    - Lazy load image

--- resources/js/Components/ppif/background/AnimatedBackground.jsx
+++ resources/js/Components/ppif/background/AnimatedBackground.jsx
@@ -21,6 +21,8 @@ export default function AnimatedBackground() {
                     className="mask-bg absolute left-0 h-full w-full object-cover opacity-80 xl:object-fill"
                     src="/ppif/images/background/umn-bg.png"
                     role="presentation"
+                    fetchPriority="low"
+                    loading="lazy"
                 />
                 {/* <img
                     className="min-w h-screen object-cover xl:w-full xl:object-fill"
