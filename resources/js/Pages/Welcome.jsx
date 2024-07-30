092b59ad967c4c58f0cc3dd2b70a1aefda5136db
michael tio <tio.michael2004@gmail.com>
2024-07-30 23:07:09 +0700

    merge punya david, dan nambah id

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -5,6 +5,7 @@ import AboutPPIF from "@/Components/ppif/about-ppif/AboutPPIF";
 import AnimatedBackground from "@/Components/ppif/background/AnimatedBackground";
 import Timeline from "@/Components/ppif/timeline/Timeline";
 import Contact from "@/Components/ppif/contact/Contact";
+import Home from "@/Components/ppif/home/Home";
 
 export default function Welcome() {
     return (
@@ -13,7 +14,7 @@ export default function Welcome() {
             <AnimatedBackground />
             <Navbar />
             <Landing id={"Home"} />
-            <Landing id={"AboutIF"} />
+            <Home id={"AboutIF"} />
             <AboutPPIF id={"AboutPPIF"} />
             <Timeline id={"Timeline"} />
             <Contact id={"Contact"} />
