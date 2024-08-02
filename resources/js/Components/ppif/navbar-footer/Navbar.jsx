b422b21887c459c9c88d97c339a112091b6ec40c
oscarjiro <oscarjiroj@gmail.com>
2024-08-03 01:43:37 +0700

    fix: adjust navbar

--- resources/js/Components/ppif/navbar-footer/Navbar.jsx
+++ resources/js/Components/ppif/navbar-footer/Navbar.jsx
@@ -37,51 +37,51 @@ export default function Navbar() {
             <div className="fixed z-50 flex h-24 w-screen justify-end md:hidden">
                 <img
                     onClick={handleBurger}
-                    className="z-50 m-6 cursor-pointer"
+                    className="glow-white z-50 m-6 cursor-pointer"
                     src="/ppif/icons/burger_navigation.svg"
                     alt=""
                 ></img>
             </div>
             <nav
-                className={`${isScrollUp ? "md:top-0" : "md:-top-40"} ${isBurgerActive ? "bg-black bg-opacity-75 backdrop-blur-md" : "pointer-events-none backdrop-blur-none"} fixed z-10 mx-auto flex h-full w-screen flex-col px-12 py-20 text-2xl text-white transition-all duration-700 ease-in-out md:pointer-events-auto md:mt-8 md:flex md:h-auto md:w-auto md:flex-row md:gap-16 md:rounded-xl md:bg-white md:bg-opacity-10 md:px-12 md:py-5 md:text-base md:shadow-[0px_0px_50px_20px_rgba(255,255,255,0.1)] md:backdrop-blur-md md:duration-200`}
+                className={`${isScrollUp ? "md:top-0" : "md:-top-40"} ${isBurgerActive ? "bg-black/30 backdrop-blur-lg" : "pointer-events-none backdrop-blur-none"} fixed z-10 mx-auto flex h-full w-screen flex-col px-12 py-20 text-2xl text-white transition-all duration-700 ease-in-out md:pointer-events-auto md:mt-8 md:flex md:h-auto md:w-auto md:flex-row md:gap-16 md:rounded-xl md:bg-white md:bg-opacity-10 md:px-12 md:py-5 md:text-base md:shadow-[0px_0px_50px_20px_rgba(255,255,255,0.1)] md:backdrop-blur-md md:duration-200`}
             >
                 <a
                     onClick={handleBurger}
-                    className={`${isBurgerActive ? "translate-y-0 delay-0" : "-translate-y-10 opacity-0 delay-[250ms]"} py-4 font-bold transition-all ease-in-out hover:drop-shadow-[0px_0px_5px_rgba(255,255,255,1)] md:translate-y-0 md:p-0 md:font-normal md:opacity-100 md:delay-0`}
+                    className={`${isBurgerActive ? "translate-y-0 delay-0" : "-translate-y-10 opacity-0 delay-[250ms]"} glow-white py-4 font-light transition-all ease-in-out hover:drop-shadow-[0px_0px_5px_rgba(255,255,255,1)] md:translate-y-0 md:p-0 md:font-normal md:opacity-100 md:delay-0`}
                     href="#Home"
                 >
                     Home
                 </a>
                 <a
                     onClick={handleBurger}
-                    className={`${isBurgerActive ? "translate-y-0 delay-[50ms]" : "-translate-y-10 opacity-0 delay-200"} py-4 font-bold transition-all ease-in-out hover:drop-shadow-[0px_0px_5px_rgba(255,255,255,1)] md:translate-y-0 md:p-0 md:font-normal md:opacity-100 md:delay-0`}
+                    className={`${isBurgerActive ? "translate-y-0 delay-[50ms]" : "-translate-y-10 opacity-0 delay-200"} glow-white py-4 font-light transition-all ease-in-out hover:drop-shadow-[0px_0px_5px_rgba(255,255,255,1)] md:translate-y-0 md:p-0 md:font-normal md:opacity-100 md:delay-0`}
                     href="#AboutIF"
                 >
                     About IF
                 </a>
                 <a
                     onClick={handleBurger}
-                    className={`${isBurgerActive ? "translate-y-0 delay-100" : "-translate-y-10 opacity-0 delay-150"} py-4 font-bold transition-all ease-in-out hover:drop-shadow-[0px_0px_5px_rgba(255,255,255,1)] md:translate-y-0 md:p-0 md:font-normal md:opacity-100 md:delay-0`}
+                    className={`${isBurgerActive ? "translate-y-0 delay-100" : "-translate-y-10 opacity-0 delay-150"} glow-white py-4 font-light transition-all ease-in-out hover:drop-shadow-[0px_0px_5px_rgba(255,255,255,1)] md:translate-y-0 md:p-0 md:font-normal md:opacity-100 md:delay-0`}
                     href="#AboutPPIF"
                 >
                     About PPIF
                 </a>
                 <a
                     onClick={handleBurger}
-                    className={`${isBurgerActive ? "translate-y-0 delay-150" : "-translate-y-10 opacity-0 delay-100"} py-4 font-bold transition-all ease-in-out hover:drop-shadow-[0px_0px_5px_rgba(255,255,255,1)] md:translate-y-0 md:p-0 md:font-normal md:opacity-100 md:delay-0`}
+                    className={`${isBurgerActive ? "translate-y-0 delay-150" : "-translate-y-10 opacity-0 delay-100"} glow-white py-4 font-light transition-all ease-in-out hover:drop-shadow-[0px_0px_5px_rgba(255,255,255,1)] md:translate-y-0 md:p-0 md:font-normal md:opacity-100 md:delay-0`}
                     href="#Timeline"
                 >
                     Timeline
                 </a>
                 <a
                     onClick={handleBurger}
-                    className={`${isBurgerActive ? "translate-y-0 delay-200" : "-translate-y-10 opacity-0 delay-[50ms]"} py-4 font-bold transition-all ease-in-out hover:drop-shadow-[0px_0px_5px_rgba(255,255,255,1)] md:translate-y-0 md:p-0 md:font-normal md:opacity-100 md:delay-0`}
+                    className={`${isBurgerActive ? "translate-y-0 delay-200" : "-translate-y-10 opacity-0 delay-[50ms]"} glow-white py-4 font-light transition-all ease-in-out hover:drop-shadow-[0px_0px_5px_rgba(255,255,255,1)] md:translate-y-0 md:p-0 md:font-normal md:opacity-100 md:delay-0`}
                     href="#Contact"
                 >
                     Contact
                 </a>
                 <hr
-                    className={`${isBurgerActive ? "translate-y-0 opacity-75 delay-[250ms]" : "-translate-y-10 opacity-0 delay-0"} mt-4 border-t-2 transition-all ease-in-out md:hidden`}
+                    className={`${isBurgerActive ? "glow-white translate-y-0 opacity-75 delay-[250ms]" : "-translate-y-10 opacity-0 delay-0"} mt-4 border-t-2 transition-all ease-in-out md:hidden`}
                 />
             </nav>
         </div>
