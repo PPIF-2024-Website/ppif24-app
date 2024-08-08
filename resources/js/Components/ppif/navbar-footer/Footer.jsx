b5c5e13309bc252b73e2fb09041d45294b31f07e
oscarjiro <oscarjiroj@gmail.com>
2024-08-09 01:12:22 +0700

    fix: move Footer.jsx

--- resources/js/Components/ppif/navbar-footer/Footer.jsx
+++ resources/js/Components/ppif/navbar-footer/Footer.jsx
@@ -1,36 +1,28 @@
+import { Link } from "@inertiajs/react";
 import React from "react";
 
-function Footer() {
-    const toGamePage = () => {
-        window.location.href = "/gameLanding";
-    };
-
+export default function Footer() {
     return (
-        <div className="relative">
-            <div className="absolute inset-0 bg-center"></div>
-
-            <div className="relative h-auto w-full bg-white bg-opacity-[40%] py-5">
-                <div className="z-10 mx-auto flex flex-col items-center justify-between lg:flex-row">
-                    <div className="mb-2 flex flex-col items-center justify-center lg:mb-0 lg:ml-6">
-                        <img
-                            src="/ppif/images/Logo_UMN_PPIF.png"
-                            alt="logo UMN X PPIF"
-                            className="lg:w-4/4 mb-5 mt-4 w-3/4 px-7 lg:scale-125 lg:transform lg:pb-4"
-                        />
-                        <h2 className="text-center text-[3.3vw] font-bold text-[#000000] lg:ml-6 lg:text-base">
-                            © 2024 Perkenalan Prodi Informatika UMN
-                        </h2>
-                    </div>
+        <footer className="w-full rounded-t-2xl bg-white/15 py-5 backdrop-blur-md">
+            <div className="z-10 mx-auto flex flex-col items-center justify-between lg:flex-row">
+                <div className="mb-2 flex flex-col items-center justify-center lg:mb-0 lg:ml-6">
                     <img
-                        src="/ppif/icons/Game_icon.png"
+                        src="/ppif/images/Logo_UMN_PPIF.svg"
+                        alt="logo UMN X PPIF"
+                        className="lg:w-4/4 mb-5 mt-4 w-3/4 px-7 lg:scale-125 lg:transform lg:pb-4"
+                    />
+                    <h2 className="text-center text-[3.3vw] font-bold text-[#ffffff] lg:ml-6 lg:text-base">
+                        © 2024 Perkenalan Prodi Informatika UMN
+                    </h2>
+                </div>
+                <Link href="/game/story">
+                    <img
+                        src="/ppif/icons/Game_icon.svg"
                         alt="Navigate to Game Page"
-                        onClick={toGamePage}
                         className="w-14 cursor-pointer lg:mr-8 lg:w-auto"
                     />
-                </div>
+                </Link>
             </div>
-        </div>
+        </footer>
     );
 }
-
-export default Footer;
