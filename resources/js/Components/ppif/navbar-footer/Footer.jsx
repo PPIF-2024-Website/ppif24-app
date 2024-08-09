57f0deef421f06bebfefb7f6c0e2d6a016d1273c
oscarjiro <oscarjiroj@gmail.com>
2024-08-10 02:17:29 +0700

    fix: finish new footer

--- resources/js/Components/ppif/navbar-footer/Footer.jsx
+++ resources/js/Components/ppif/navbar-footer/Footer.jsx
@@ -1,28 +1,38 @@
 import { Link } from "@inertiajs/react";
 import React from "react";
+import { FolderOpenIcon } from "@heroicons/react/24/outline";
+import "./Footer.css";
 
 export default function Footer() {
     return (
-        <footer className="w-full rounded-t-2xl bg-white/15 py-5 backdrop-blur-md">
-            <div className="z-10 mx-auto flex flex-col items-center justify-between lg:flex-row">
-                <div className="mb-2 flex flex-col items-center justify-center lg:mb-0 lg:ml-6">
+        <footer className="flex w-full items-center justify-between rounded-t-xl bg-gradient-to-t from-black/50 to-transparent px-16 py-5 text-white backdrop-blur-md">
+            {/* LOGO AND FOOTER TEXT */}
+            <div className="flex w-full flex-col items-center space-y-5 md:items-start">
+                {/* LOGO */}
+                <div className="flex items-center space-x-8">
                     <img
-                        src="/ppif/images/Logo_UMN_PPIF.svg"
-                        alt="logo UMN X PPIF"
-                        className="lg:w-4/4 mb-5 mt-4 w-3/4 px-7 lg:scale-125 lg:transform lg:pb-4"
+                        src="/ppif/images/footer/umn-logo.svg"
+                        alt=""
+                        className="w-[80px] min-[380px]:h-[30px] min-[380px]:w-auto min-[410px]:h-[40px] min-[490px]:h-[50px]"
                     />
-                    <h2 className="text-center text-[3.3vw] font-bold text-[#ffffff] lg:ml-6 lg:text-base">
-                        © 2024 Perkenalan Prodi Informatika UMN
-                    </h2>
+                    <img
+                        src="/ppif/images/footer/ppif2024-logo.svg"
+                        alt=""
+                        className="w-[130px] min-[380px]:h-[25px] min-[380px]:w-auto min-[410px]:h-[30px] min-[490px]:h-[40px]"
+                    />
                 </div>
-                <Link href="/game/story">
-                    <img
-                        src="/ppif/icons/Game_icon.svg"
-                        alt="Navigate to Game Page"
-                        className="w-14 cursor-pointer lg:mr-8 lg:w-auto"
-                    />
-                </Link>
+
+                {/* TEXT */}
+                <p className="text-footer text-center font-light tracking-tight">
+                    © 2024 Perkenalan Prodi Informatika UMN. All rights
+                    reserved.
+                </p>
             </div>
+
+            {/* GAME LINK */}
+            <Link href="/game/story" className="hidden md:block">
+                <FolderOpenIcon className="glow-white w-[75px] animate-[pulse_3s_ease-out_infinite] opacity-100 hover:animate-[shake_1s_ease-in_infinite] [&>path]:stroke-[0.4]" />
+            </Link>
         </footer>
     );
 }
