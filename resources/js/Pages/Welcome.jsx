338151a44e5c4860b2d7ff1ef6e751c011110d82
michael tio <tio.michael2004@gmail.com>
2024-08-13 22:17:28 +0700

    Update Welcome.jsx

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -19,6 +19,7 @@ function Page() {
             <AboutPPIF id={"about-ppif"} />
             <Timeline id={"timeline"} />
             <Contact id={"contact"} />
+            <div>Halo ini test</div>
         </>
     );
 }
